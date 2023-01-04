98bcd75063337f56bbb4c4dac7855eaadf637409
nehal vyas <114698016+jentibhai@users.noreply.github.com>
2023-01-04 00:22:04 +0000

    Initial commit

---
+++ src/main/java/com/petstore/constants/EndPoints.java
@@ -0,0 +1,28 @@
+package com.petstore.constants;
+
+/**
+ * Created by Jay
+ */
+public class EndPoints {
+    /**
+     * This is Endpoints of Product api
+     */
+    public static final String GET_ALL_PET = "/pet";
+    public static final String GET_SINGLE_PET_BY_ID = "/{id}";
+    public static final String UPDATE_PET_BY_ID = "/{id}";
+    public static final String DELETE_PET_BY_ID = "/{id}";
+
+    /**
+     * This is Endpoints of Store api
+     */
+    public static final String GET_ALL_USERS = "/user";
+
+    public static final String GET_SINGLE_USER_BY_NAME = "/{name}";
+
+    public static final String UPDATE_USER_BY_NAME="/{name}";
+
+    public static final String DELETE_USER_BY_NAME = "/{name}";
+
+
+
+}
---
+++ src/main/java/com/petstore/constants/Path.java
@@ -0,0 +1,14 @@
+package com.petstore.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+    /*
+     * This is base path for student api
+     */
+    public static final String PET= "/pet";
+
+    public static final String USER= "/user";
+
+}
